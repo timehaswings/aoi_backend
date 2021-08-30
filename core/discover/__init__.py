e85a18f798b42d6e8462c8b73a4d8547b68ecc7d
王刚 <wanggang6595@cvte.com>
2021-08-30 09:20:47 +0800

    [feature] 修改author

--- core/discover/__init__.py
+++ core/discover/__init__.py
@@ -2,5 +2,5 @@
 # !/usr/bin/env python
 
 # @Time    : 2021/8/28 8:55
-# @Author  : wanggang6595
+# @Author  : NoWords
 # @FileName: __init__.py.py
--- core/statistics/__init__.py
+++ core/statistics/__init__.py
@@ -2,5 +2,5 @@
 # !/usr/bin/env python
 
 # @Time    : 2021/8/26 12:13
-# @Author  : wanggang6595
+# @Author  : NoWords
 # @FileName: __init__.py.py
