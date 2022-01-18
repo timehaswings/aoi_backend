c5de02e135edcc715eeb2637ce5c47dbf2d6994a
“lnowordss <timehaswingss@gmail.com>
2022-01-18 22:44:55 +0800

    [feature] improve some api

--- core/home/category_view.py
+++ core/home/category_view.py
@@ -5,3 +5,58 @@
 # @Author  : NoWords
 # @FileName: category_view.py
 
+from ..serializers import CategorySerializer, TagsSerializer
+from ..models import Category, Tags
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CategoryApiView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, *args, **kwargs):
+        filters = {'is_delete': 0, 'is_active': 1}
+        category = Category.objects.filter(**filters).order_by('sort')
+        category = CategorySerializer(category, many=True).data
+        result = {
+            'msg': '获取成功',
+            'success': True,
+            'data': category
+        }
+        return Response(result, status.HTTP_200_OK)
+
+
+class TagsApiView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, *args, **kwargs):
+        filters = {'is_delete': 0, 'is_active': 1}
+        tags = Tags.objects.filter(**filters).order_by('sort')
+        tags = TagsSerializer(tags, many=True).data
+        result = {
+            'msg': '获取成功',
+            'success': True,
+            'data': tags
+        }
+        return Response(result, status.HTTP_200_OK)
+
+
+class ApiView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, *args, **kwargs):
+        filters = {'is_delete': 0, 'is_active': 1}
+
+        result = {
+            'msg': '获取成功',
+            'success': True,
+            'data': []
+        }
+        return Response(result, status.HTTP_200_OK)
--- core/home/index_view.py
+++ core/home/index_view.py
@@ -24,14 +24,14 @@ class CarouselApiView(APIView):
     authentication_classes = []
 
     def get(self, request, *args, **kwargs):
-        filters = {'is_delete': 0, }
+        filters = {'is_delete': 0, 'is_active': 1}
         count = BaseVideo.objects.filter(**filters).count()
         limit = 6  # 数据量
         if count <= limit:
-            base_video = BaseVideo.objects.filter(**filters)
+            base_video = BaseVideo.objects.filter(**filters).order_by('sort')
         else:
             start = randint(0, count - limit)
-            base_video = BaseVideo.objects.filter(**filters)[start: start + limit]
+            base_video = BaseVideo.objects.filter(**filters).order_by('sort')[start: start + limit]
         base_video = BaseVideoSerializer(base_video, many=True).data
         result = {
             'msg': '获取成功',
@@ -50,26 +50,25 @@ class CategoryVideoApiView(APIView):
 
     def get(self, request, *args, **kwargs):
         # 获取随机分类
-        category_filters = {'is_delete': 0}
+        category_filters = {'is_delete': 0, 'is_active': 1}
         category_count = Category.objects.filter(**category_filters).count()
         category_limit = 5
         if category_count <= category_limit:
-            category = Category.objects.filter(**category_filters)
+            category = Category.objects.filter(**category_filters).order_by('sort')
         else:
             start = randint(0, category_count - category_limit)
-            category = Category.objects.filter(**category_filters)[start: start + category_limit]
+            category = Category.objects.filter(**category_filters).order_by('sort')[start: start + category_limit]
         category = CategorySerializer(category, many=True).data
         # 获取分类下的视频
         video_limit = 8
         for item in category:
-            video_filter = {'category_id': item['id'], 'is_delete': 0}
+            video_filter = {'category_id': item['id'], 'is_delete': 0, 'is_active': 1}
             video_count = BaseVideo.objects.filter(**video_filter).count()
-            print(video_count)
             if video_count <= video_limit:
-                base_video = BaseVideo.objects.filter(**video_filter)
+                base_video = BaseVideo.objects.filter(**video_filter).order_by('sort')
             else:
                 start = randint(0, video_count - video_limit)
-                base_video = BaseVideo.objects.filter(**video_filter)[start: start + video_limit]
+                base_video = BaseVideo.objects.filter(**video_filter).order_by('sort')[start: start + video_limit]
             base_video = BaseVideoSerializer(base_video, many=True).data
             item['video_list'] = base_video
         result = {
