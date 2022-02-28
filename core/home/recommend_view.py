d08015918ae90a8debd4ae8162ab3dc8d0520fda
王刚 <wanggang6595@cvte.com>
2022-02-28 09:42:13 +0800

    [feature] complete recommend api

--- core/home/recommend_view.py
+++ core/home/recommend_view.py
@@ -4,8 +4,10 @@
 # @Time    : 2022/2/26 9:24
 # @Author  : nowords
 # @FileName: recommend_view.py
+import random
 
-from ..models import Category, Tags, Area, BaseVideo
+from core.serializers import BaseVideoSerializer
+from ..models import Tags, BaseVideo
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,14 +22,25 @@ class RecommendApiView(APIView):
         params = request.GET
         vid = params.get('id')
         if not vid:
-            return Response({msg: '参数错误', success: False}, status.HTTP_200_OK)
+            return Response({'msg': '参数错误', 'success': False}, status.HTTP_200_OK)
+        limit = params.get('limit', 6)
         video = BaseVideo.objects.filter(id=vid, is_delete=0, is_active=1).first()
         if not video:
-            return Response({msg: '视频未找到', success: False}, status.HTTP_200_OK)
-        print(video.tags)
+            return Response({'msg': '视频未找到', 'success': False}, status.HTTP_200_OK)
+        tags_list = video.tags.all()
+        if not tags_list:
+            return Response({'msg': '视频未设置标签', 'success': False}, status.HTTP_200_OK)
+        index = random.randint(0, tags_list.count()-1)
+        tags = Tags.objects.filter(id=tags_list[index].id).first()
+        video_list = tags.basevideo_set.all()
+        if video_list.count() <= limit:
+            rows = BaseVideoSerializer(video_list, many=True).data
+        else:
+            start = random.randint(0, video_list.count()-limit)
+            rows = BaseVideoSerializer(video_list[start, limit], many=True).data
         result = {
             'msg': '获取成功',
             'success': True,
-            'data': []
+            'data': rows
         }
         return Response(result, status.HTTP_200_OK)
